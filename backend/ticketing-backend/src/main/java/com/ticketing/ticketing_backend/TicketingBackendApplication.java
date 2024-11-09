522a6497cf42a41750c1ee83d3eee78965c4f1aa
Rikas Ilamdeen <rikasilamdeen@gmail.com>
2024-11-10 01:31:05 +0530

    Initial commit

---
+++ backend/ticketing-backend/src/main/java/com/ticketing/ticketing_backend/TicketingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ticketing.ticketing_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingBackendApplication.class, args);
+	}
+
+}
